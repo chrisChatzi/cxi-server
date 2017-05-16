7469cec92c8b3d2e65a4adc45f5c89a71388dc44
chrisChatzi <christos.chatziioannidis@gmail.com>
2017-05-16 18:29:56 +0200

    send message functionality added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express'),
     path = require('path'),
     app = require('express')();
     http = require('http').Server(app),
+    bodyParser = require('body-parser'),
+    mail = require('nodemailer'),
     httpPort = 8080;
 
 httpServerFunction();
@@ -12,6 +14,13 @@ httpServerFunction();
         app.use('/tech', express.static((path.join(__dirname,'./dist'))));
         app.use('/work', express.static((path.join(__dirname,'./dist'))));
 
+        app.use(bodyParser.json({limit: "50mb"}));
+        app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+        app.post('/sendMsg', function (req, res) {
+            sendMsg(req.body);
+            res.send(JSON.stringify({ res : true }));
+        });
         ///////
         app.set('port', (process.env.PORT || 5000));
 
@@ -25,3 +34,34 @@ httpServerFunction();
 		});
         //////
     };
+
+    //send email to trader
+    function sendMsg(data){
+        var html = "";
+        html += "<h3>CXI Message</h3>";
+        html += "<p><b>From:</b> "+data.name+"</p>";
+        html += "<p><b>Email:</b> "+data.email+"</p>";
+        html += "<br/>";
+        html += "<p>"+data.txt+"</p>";
+        let transporter = mail.createTransport({
+            service: 'yahoo',
+            auth: {
+                user: 'xchris777@yahoo.com',
+                pass: 'ca21d3yh7'
+            }
+        });
+        let mailOptions = {
+            from: 'Christos<xchris777@yahoo.com>',
+            to: 'xchris777@yahoo.com',
+            subject: 'CXI message: '+data.title,
+            text: data.name+" "+data.txt,
+            html: html
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+    }
